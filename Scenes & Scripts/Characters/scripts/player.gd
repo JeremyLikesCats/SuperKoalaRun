06f7d76b5e9f154ba4f0165f3b801a464d1d0558
Jeremy Zhou <112549553+JeremyLikesCats@users.noreply.github.com>
2025-09-20 14:42:35 +0800

    new art

--- Scenes & Scripts/Characters/scripts/player.gd
+++ Scenes & Scripts/Characters/scripts/player.gd
@@ -64,7 +64,8 @@ func move(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, Input.get_axis("Left", "Right") * MAX_SPEED * delta, MOVE_THRUST * delta * JUMP_ACCELERATION_FACTOR)
 		if Input.is_action_pressed("Jump") and jumping:
 			velocity.y = -HOLD_JUMP_SPEED
-
+		if not Input.is_action_pressed("Jump"):
+			jumping = false
 		
 func knockback_detection(delta: float) -> void:
 	if not knockback_timer.is_stopped():
